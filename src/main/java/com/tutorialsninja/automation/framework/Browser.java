65b5b3310d99a6b549a4b5590ca89459d9d45d1b
MadhuriSarode <madhuri.sarode14@gmail.com>
2020-07-15 15:21:16 +0900

    My Account Code Working

--- src/main/java/com/tutorialsninja/automation/framework/Browser.java
+++ src/main/java/com/tutorialsninja/automation/framework/Browser.java
@@ -23,7 +23,7 @@ public class Browser {
 		switch (browser) {
 
 		case "chrome":
-			WebDriverManager.chromedriver().setup();
+			WebDriverManager.chromedriver().version("83").setup();
 			Base.driver = new ChromeDriver();
 			log.info("Chrome Browser is Started" + Base.driver.hashCode());
 			return Base.driver;
